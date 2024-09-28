e071de9cd35f7f937c44219cbe7049f4e9050834
Luca Irimie <lucaniri@umich.edu>
2024-09-28 19:15:16 -0400

    api url update

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Image, Text, TextInput, TouchableOpacity, Button } from 'react-native';
 import { useState, useEffect } from 'react';
@@ -5,28 +6,12 @@ import { MaterialIcons } from '@expo/vector-icons'; // Importing icon library
 import { Camera, CameraType, useCameraPermissions, CameraView } from 'expo-camera'; // Camera imports
 import axios from 'axios';
 
-const baseUrl = 'https://api.edamam.com';
 
-// axios.get('https://api.edamam.com/api/recipes/v2?type=public&q=beef%2C%20broccoli%2C%20ginger%2C%20onion&app_id=4a1c77c0&app_key=ee005e4f5ba45324c68ca32635e02f32')
-//   .then(response => {
-//     console.log(response.data);
-//   })
-//   .catch(error => {
-//     console.error("Error fetching data: ", error);
-//   });
+axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=beef%2C%20broccoli%2C%20ginger%2C%20onion&app_id=4a1c77c0&app_key=ee005e4f5ba45324c68ca32635e02f32`, {
 
-axios({
-    method: 'get',
-    url: `${baseUrl}/api/recipes/v2`,
-  }).then((response) => {
-    console.log(response.data);
-  });
-
-axios.get(`${baseUrl}/api/recipes/v2`, {
-     
-  }).then((response) => {
-    console.log(response.data);
-  });
+}).then((response) => {
+  console.log(response.data);
+});
 
 
 const PlaceholderImage = require('../assets/images/lunch.png');
@@ -71,7 +56,7 @@ export default function App() {
           <TextInput
             style={styles.searchBar}
             placeholder="Search for recipes..."
-            placeholderTextColor="#aaa" 
+            placeholderTextColor="#aaa"
           />
           <StatusBar style="auto" />
         </>
