c8381303d6d39e1a475d496c7131c91163bcdfb0
Luca Irimie <lucaniri@umich.edu>
2024-09-28 19:32:47 -0400

    personalize api key + id

--- app/index.tsx
+++ app/index.tsx
@@ -5,10 +5,13 @@ import { useState, useEffect } from 'react';
 import { MaterialIcons } from '@expo/vector-icons'; // Importing icon library
 import { Camera, CameraType, useCameraPermissions, CameraView } from 'expo-camera'; // Camera imports
 import axios from 'axios';
+import { format } from 'react-string-format';
+
+const api_key = 'ee005e4f5ba45324c68ca32635e02f32';
+const id = '4a1c77c0';
 
 
-axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=beef%2C%20broccoli%2C%20ginger%2C%20onion&app_id=4a1c77c0&app_key=ee005e4f5ba45324c68ca32635e02f32`, {
-
+axios.get(format("https://api.edamam.com/api/recipes/v2?type=public&app_id={0}&app_key={1}", id, api_key), {
 }).then((response) => {
   console.log(response.data);
 });
