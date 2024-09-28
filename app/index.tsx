e572d19d03b124b4e13a194f3a28828f3b976029
Luca Irimie <lucaniri@umich.edu>
2024-09-28 18:21:37 -0400

    axios api update

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,28 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 import axios from 'axios';
 
-axios.get('https://api.edamam.com/api/recipes/v2?type=public&q=beef%2C%20broccoli%2C%20ginger%2C%20onion&app_id=4a1c77c0&app_key=ee005e4f5ba45324c68ca32635e02f32')
-  .then(response => {
+const baseUrl = 'https://api.edamam.com';
+
+// axios.get('https://api.edamam.com/api/recipes/v2?type=public&q=beef%2C%20broccoli%2C%20ginger%2C%20onion&app_id=4a1c77c0&app_key=ee005e4f5ba45324c68ca32635e02f32')
+//   .then(response => {
+//     console.log(response.data);
+//   })
+//   .catch(error => {
+//     console.error("Error fetching data: ", error);
+//   });
+
+  axios({
+    method: 'get',
+    url: `${baseUrl}/api/recipes/v2`,
+  }).then((response) => {
     console.log(response.data);
-  })
-  .catch(error => {
-    console.error("Error fetching data: ", error);
   });
 
+axios.get(`${baseUrl}/api/recipes/v2`, {
+     
+  }).then((response) => {
+    console.log(response.data);
+  });
 
 export default function Index() {
   return (
@@ -27,4 +41,6 @@ export default function Index() {
   );
 }
 
+const styles = StyleSheet.create({
 
+});
